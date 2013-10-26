bf91fbd09641a744622fe7fd33d2d3b5bfe4c8ec
Dandalf <daniel@casserlyprogramming.com>
2013-10-26 23:46:03 +0100

    Nearly finished with the form. Just need to test and fix the issues that are there

--- cpshortcodes.php
+++ cpshortcodes.php
@@ -66,4 +66,80 @@ function tickets_grid_sc($atts)
 }
 add_shortcode('cp_tickets_grid', 'tickets_grid_sc');
 
+function ticket_add_sc($atts)
+{
+    $result = '';
+    ob_start();
+    
+    // Get the lists of taxonomies...
+    $types = get_terms('cp_ticket_types', array('hide_empty' => false));
+    $products = get_terms('cp_support_product', array('hide_empty' => false));
+    $statuses = get_terms('cp_ticket_status', array('hide_empty' => false));
+    
+    echo
+    '<form method="POST" action="'. plugins_url( 'api/insert_ticket.php' , __FILE__ ) . '">
+       <table> 
+        <tr>
+            <td>
+            <label for="edTitle">Title</label>
+            </td>
+            <td>
+            <input type="text" name="edTitle"/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <label for="edDetails">Details</label>
+            </td>
+            <td>
+            <textarea rows="4" cols="50" name="edDetails"></textarea>
+	</td>
+        </tr>
+        <tr>
+            <td>
+            <label for="edProduct">Product</label>
+            </td>
+            <td>
+            <select name="edProduct">';
+            
+    foreach($products as $prod)
+    {
+        echo '<option value="' . $prod->slug . '">' . $prod->name . '</option>';
+    }
+
+    echo   '</select>
+	</td>
+        </tr>
+        <tr>
+            <td>
+            <label for="edStatus">Status</label>
+             </td>
+            <td>
+            <select name="edStatus">';
+    foreach($statuses as $stat)
+    {
+        echo '<option value="' . $stat->slug . '">' . $stat->name . '</option>';
+    }
+    
+    echo '</select>
+	</td>
+        </tr>
+        <tr>
+            <td>
+            <label for="edType">Type</label>
+             </td>
+            <td>
+            <select name="edType">';
+    foreach($types as $typ)
+    {
+        echo '<option value="' . $typ->slug . '">' . $typ->name . '</option>';
+    }
+    echo '  </select>
+	</td>
+        </tr></table>
+<input type="submit" value="Create Ticket"/>        
+</form>';
+    return $result;
+}
+add_shortcode('cp_ticket_add', 'ticket_add_sc');
 ?>
